7fb9161ba35bea47953d27a27784bbe19ca9ee49
Clouds Flowing <clouds.flowing@gmail.com>
2021-02-02 18:06:49 +0800

    add vitejs-fs-server

---
+++ examples/default.md
@@ -0,0 +1,5 @@
+Welcome to NodeBook
+------
+1. node is note
+2. link likes node
+3. note-like wiki
--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,12 @@
 import vue from '@vitejs/plugin-vue'
+import { serveDir, fsPlugin } from '@vitejs/fs-server'
+import { defineConfig } from 'vite'
 
-export default {
-  plugins: [vue()]
-}
+export default defineConfig({
+  plugins: [
+    vue(),
+    fsPlugin({
+      '/@data': serveDir('examples'),
+    })
+  ],
+})
